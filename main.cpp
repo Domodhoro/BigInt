fb8c7db303fb0c2b184b4362d4aef7c21c6b640f
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-11-02 19:16:42 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 #include <stdexcept>
 
-using std::cout;
-using std::cerr;
-using std::cin;
-using std::exception;
-
 #include "bigint.hpp"
 
 int main() {
 	try {
-        BigInt x("10000000000000000000000000000000000000000"), y("1"), z;
+        Math::BigInt x {"1000000"}, y {"1"};
 
-        z = x - y;
+        auto z {x - y};
 
-        cout << "x + y = " << z;
-        cin.get();
+        std::cout << "z = " << z;
+        std::cin.get();
 
         return 0;
-	} catch (const exception& e) {
-        cerr << e.what();
-        cin.get();
+	} catch (const std::exception& e) {
+        std::cerr << e.what();
+        std::cin.get();
 
         return 1;
 	}
